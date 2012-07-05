112ae2e583237b3ff42b436fc16595053ba70100
Douglas Edmonson <dedmons@tarazet.dreamhost.com>
2012-07-05 13:10:20 -0700

    Setup .htaccess file for symlinking from a /v1/ dir

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://apis.dedmonson.com';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
