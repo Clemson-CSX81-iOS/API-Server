8afdf653b5263c04f6eed57f0630f2941490c9dd
DJ Edmonson <dedmons@g.clemson.edu>
2012-07-05 20:22:55 -0700

    alot of changes to add creating, getting, and deleting records

--- application/controllers/ip.php
+++ application/controllers/ip.php
@@ -6,21 +6,65 @@ require(APPPATH.'/libraries/REST_Controller.php');
 class IP extends REST_Controller
 {
 
+	public function server_get()
+	{
+		print_r($_SERVER);
+	}
+
 	public function mine_get()
 	{
 		$this->response(array('CLIENT_IP' => $this->input->server('REMOTE_ADDR')),201);
 	}
 	
-	public function report_get()
+	public function records_get()
 	{
-		
+		$jsonObj = read_file('./data.json');
+		if ($jsonObj) {
+			$jsonObj = json_decode($jsonObj,true);
+			$this->response($jsonObj,201);
+		}
+		else
+			$this->response(array('MESSAGE'=>'No records on server'),201);
 	}
+	
+	public function records_delete(){
+		if ($this->delete('check')){
+			$this->response(array('MESSAGE'=>'Records deleted'),201);
+		}
+		$this->response(array('MESSAGE'=>$_SERVER),202);
+	}
+	
+	public function records_post(){
+		// if ($this->post('check')){
+			// $this->response(array('MESSAGE'=>'Records deleted'),201);
+		// }
+		$this->response(array('MESSAGE'=>$_SERVER),202);
+	}
+	
 	public function report_post()
 	{
-		$ip = "none";
-		if (!$this->input->post('ip')){
-			$ip = $this->input->post('ip');
+		$host =  $this->post('host');
+		$user = $this->post('user');
+		
+		$ip = $this->input->server('REMOTE_ADDR');
+		if ($this->post('ip')){
+			$ip = $this->post('ip');
 		}
-		$this->response(array('RECIVED_HOST' => $this->input->post('host'),'RECIVED_IP' => $ip),201);
+		
+		$response = array('RECIVED_HOST' => $host,'HOST_IP' => $ip, 'USER' => $user);
+		
+		$jsonObj = read_file('./data.json');
+		if ($jsonObj) {
+			$jsonObj = json_decode($jsonObj,true);
+		}
+		
+		$jsonObj[$user] = $response;
+		
+		$success = write_file('./data.json',json_encode($jsonObj));
+		
+		$response['SUCCESS'] = (!$success) ? "NO" : "YES";
+		$response['INFO'] = $this->input->server('REDIRECT_SCRIPT_URI');
+		
+		$this->response($response,201);
 	}
 }
